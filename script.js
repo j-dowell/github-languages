eecf5558d5b8f7ea1719b78ca1ea05d0e01b932f
James Dowell <james.s.dowell@gmail.com>
2018-11-05 21:23:33 -0800

    added comments and fixed indendation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const rp = require('request-promise');
 const githubUser = process.env.GITHUB_USER;
 const token = process.env.TOKEN;
 
+// Get list of user's repos
 const getRepos = (username) => {
   let repoList = [];
   const options = {
@@ -27,6 +28,7 @@ const getRepos = (username) => {
     });
 }
 
+// Given a repo and user, returns JSON containing repo/language stats
 const getLanguage = (repo, username) => {
   const options = {
     url: `https://api.github.com/repos/${username}/${repo}/languages?access_token=${token}`,
@@ -36,27 +38,28 @@ const getLanguage = (repo, username) => {
   }
 
   return rp(options)
-  .then((result) => {
-    const languages = JSON.parse(result);
-    // Get total codebase size
-    const totalBytes = Object.values(languages).reduce((a, b) => a + b);
-    let percentageInformation = {};
-    // Calculating percantage of each language in repo
-    for (let language in languages) {
-      percentageInformation[language] = parseFloat(((languages[language] / totalBytes) * 100).toFixed(2));
-    }
-    return {
-      repo,
-      languages,
-      totalBytes,
-      percentageInformation
-    }
-    })
-    .catch((err) => {
-        // API call failed...
-    });
+    .then((result) => {
+      const languages = JSON.parse(result);
+      // Get total codebase size
+      const totalBytes = Object.values(languages).reduce((a, b) => a + b);
+      let percentageInformation = {};
+      // Calculating percantage of each language in repo
+      for (let language in languages) {
+        percentageInformation[language] = parseFloat(((languages[language] / totalBytes) * 100).toFixed(2));
+      }
+      return {
+        repo,
+        languages,
+        totalBytes,
+        percentageInformation
+      }
+      })
+      .catch((err) => {
+          // API call failed...
+      });
 }
 
+// Looping over all of user's repos and retreiving language info
 const getAllRepoLanguages = (repoList) => {
   repoList.forEach(res => {
     getLanguage(res, githubUser).then(res => console.log(res))
